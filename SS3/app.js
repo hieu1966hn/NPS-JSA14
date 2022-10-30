7ed5e6cd5d1edac5188ed6277c2f877d53915c0d
hieu1966hn <hieu1966hn@gmail.com>
2022-10-30 19:16:15 +0700

    new code SS3

---
+++ SS3/app.js
@@ -0,0 +1,19 @@
+/*
+Buổi 2: 
+- Kiến thức sơ lược về Javascript
+*/
+
+////// Giới thiệu thêm về vòng lặp trong đó
+/*
+for in: duyệt mảng
+
+for of: duyệt mảng
+*/
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+///// In ra từng phần tử mảng
+
+////  C1: dùng for 
+for (let i = 0; i < arr.length; i++) {
+  console.log(i);
+}
