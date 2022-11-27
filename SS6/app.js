a92f34b9500b8b7231891150053615e92a222b22
hieu1966hn <hieu1966hn@gmail.com>
2022-11-27 20:29:32 +0700

    Done bài toán menu local Storage

--- SS6/app.js
+++ SS6/app.js
@@ -8,21 +8,21 @@
 */
 
 /////// Khởi tạo Khóa và giá trị cho LS: 
-let arr = [1, 2, 3];
-localStorage.setItem("key", JSON.stringify(arr));
+// let arr = [1, 2, 3];
+// localStorage.setItem("key", JSON.stringify(arr));
 
-///// lấy dữ liệu từ LS về: 
-console.log(
-  JSON.parse(localStorage.getItem("key"))
-
-
-);
-
-console.log(arr);
+// ///// lấy dữ liệu từ LS về: 
+// console.log(
+//   JSON.parse(localStorage.getItem("key"))
 
 
-///// Update dữ liệu: 
-localStorage.setItem("key", "Hello World");
+// );
+
+// console.log(arr);
+
+
+// ///// Update dữ liệu: 
+// localStorage.setItem("key", "Hello World");
 
 //// Xóa khóa Key đi;
 // localStorage.removeItem("key")
@@ -38,3 +38,52 @@ Bài toán nhà hàng: Một nhà hàng đang xây dựng ứng dụng order men
 4. U: Update => Hiển thị yêu cầu "Mời người dùng nhập vào món ăn muốn update" => hiển thị yêu cầu "Mời người dùng nhập tên món ăn mới" => Lưu trữ mới nhất các món ăn trên LS
 5. D: Delete => Hiển thị yêu cầu "Mời người dùng nhập vào món ăn muốn Delete" => Lưu trữ mới nhất các món ăn trên LS
 */
+
+
+///// Bài chữa
+let input;
+let menu = [];
+///// Chống reset localStorage
+if (localStorage.menu) {
+  menu = JSON.parse(localStorage.getItem("menu"))
+} else {
+  localStorage.setItem("menu", JSON.stringify(menu))
+}
+
+do {
+  input = prompt("Mời người dùng nhập vào C,R,U,D").trim().toLowerCase();
+  if (input == "c") {
+    let newFood = prompt("Mời người dùng nhập vào tên món ăn mới").trim();
+    menu.push(newFood);
+
+    /// Đẩy món ăn mới lên localStorage
+    localStorage.setItem("menu", JSON.stringify(menu))
+    console.log("chạy code");
+  }
+  else if (input == "r") {
+    alert(menu)
+  }
+  else if (input == "u") {
+    let findFood = prompt("Mời người dùng nhập vào tên món ăn muốn update").trim();
+
+    /// Tìm vị trí phần tử trong mảng
+    let numberOfFood = menu.indexOf(findFood);
+    console.log("numberOfFood: ", numberOfFood);
+    menu[numberOfFood] = prompt("Mời người dùng nhập món ăn mới được update");
+    console.log(menu);
+    localStorage.setItem("menu", JSON.stringify(menu));
+  }
+  else if (input == "d") {
+    let foodDelete = prompt("Mời người dùng nhập vào tên món ăn muốn delete").trim();
+    let numberFoodDelete = menu.indexOf(foodDelete);
+    menu.splice(numberFoodDelete, 1);
+    localStorage.setItem("menu", JSON.stringify(menu));
+  }
+  else if (input == "q") {
+    break; // thoát khỏi vòng lặp ngay lập tức
+  }
+  else {
+    alert("Mời người dùng nhập lại!!");
+  }
+}
+while (input != "c" && input != "r" && input != "u" && input != "d");
