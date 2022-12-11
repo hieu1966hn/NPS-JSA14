a6c30de00e2fab9238adc9e931eaf1b1461f2567
hieu1966hn <hieu1966hn@gmail.com>
2022-12-11 19:41:24 +0700

    new code index.js

---
+++ SS7+SS8/js/index.js
@@ -0,0 +1,16 @@
+const searchInput = document.getElementById("search-input");
+
+const cityName = document.querySelector(".city-name");
+
+const weatherState = document.querySelector(".weather-state");
+const weatherIcon = document.querySelector(".weather-icon");
+const temperature = document.querySelector(".temperature");
+
+const sunrise = document.querySelector('.sunrise');
+const sunset = document.querySelector('.sunset');
+
+const humidity = document.querySelector('.humidity');
+const windSpeed = document.querySelector('.wind-speed');
+
+
+
